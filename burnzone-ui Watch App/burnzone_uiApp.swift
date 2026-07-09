96c9b3545c5d931eb0f89245347521872493260c
Christian Matsoukis <cmatsoukis@gmail.com>
2026-07-09 16:19:46 -0400

    Initial Commit

---
+++ burnzone-ui Watch App/burnzone_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  burnzone_uiApp.swift
+//  burnzone-ui Watch App
+//
+//  Created by Christian Matsoukis on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct burnzone_ui_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ burnzone-ui Watch AppTests/burnzone_ui_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  burnzone_ui_Watch_AppTests.swift
+//  burnzone-ui Watch AppTests
+//
+//  Created by Christian Matsoukis on 7/9/26.
+//
+
+import Testing
+@testable import burnzone_ui_Watch_App
+
+struct burnzone_ui_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
